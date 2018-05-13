123881b1d1424c50d25887350073cea24547c464
mahito17 <38057665+mahito17@users.noreply.github.com>
2018-05-12 20:53:11 -0500

    NuevoUsuario

---
+++ Interfaces/Interfaces/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Interfaces
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() => InitializeComponent();
+
+
+        private void Clicked_login(object sender, EventArgs e)
+
+
+        {
+            if (txtusuario.Text == "admin" && txtcontrasena.Text == "admin")
+            {
+
+                Navigation.PushModalAsync(new Menu());
+
+            }
+
+            else
+            {
+                DisplayAlert("Usuario No Registrado", "OK");
+            }
+        }
+
+        private void DisplayAlert(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
+
---
+++ Interfaces/Interfaces/UsuarioNuevo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Interfaces
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class UsuarioNuevo : ContentView
+	{
+		public UsuarioNuevo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
